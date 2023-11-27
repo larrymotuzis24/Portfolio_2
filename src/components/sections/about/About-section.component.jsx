88964e59a20bb4bbb8a6fd3075909e90d108db26
Larry Motuzis <mlauris2006@yahoo.com>
2023-11-27 15:08:37 -0600

    updated about styles

--- src/components/sections/about/About-section.component.jsx
+++ src/components/sections/about/About-section.component.jsx
@@ -1,9 +1,12 @@
 // AboutSection.jsx
 import React from 'react';
-import { Link } from 'react-scroll';
 import profilePic from "./IMG_0210.JPG";
-import { AboutContainer, AboutContent, ProfileImage, TechnologiesList, TechnologiesTitle, AboutText } from './about.styles';
+import { AboutContainer, AboutContent, ProfileImage, TechnologiesList, TechnologiesTitle, AboutText, Icon, TechnologyItem, TechbologyListsContainer, TechnologiesSubTitle} from './about.styles';
 import Header from '../../header/Header-componet';
+import { FaReact, FaNodeJs, FaPhp } from 'react-icons/fa';
+import { SiMongodb, SiTailwindcss  } from "react-icons/si";
+import { BiLogoRedux, BiLogoFirebase } from "react-icons/bi";
+import { TbBrandTypescript } from "react-icons/tb";
 
 const AboutSection = () => {
   const aboutMe = `
@@ -14,23 +17,77 @@ const AboutSection = () => {
     Through dedication and perseverance, I've honed my abilities in various programming languages and frameworks, constantly striving to push the boundaries of what's possible. I believe that software development and Web development is an ever-evolving art form, and I'm committed to growing alongside it. I have done work in software and web development, front-end/back-end web and database/server management.
   `;
 
+  
+
   return (
     <AboutContainer>
       <Header />
     <AboutContent>
         <AboutText>{aboutMe}</AboutText>
         <ProfileImage src={profilePic} alt="profile" />
-        <TechnologiesTitle>Technologies I've been working with</TechnologiesTitle>
-        <TechnologiesList>
-          <li>JavaScript</li>
-          <li>React</li>
-          <li>Redux-Toolkit</li>
-          <li>Node.js</li>
-          <li>TypeScript</li>
-          <li>PHP</li>
-          <li>MongoDB</li>
-          <li>Firebase</li>
-        </TechnologiesList>
+        <TechnologiesTitle> Technologies I've been working with </TechnologiesTitle>
+      <TechbologyListsContainer>
+        <div style={{ marginRight: '20px' }}>
+          <TechnologiesSubTitle> Frontend </TechnologiesSubTitle>
+          <TechnologiesList>
+            <TechnologyItem> 
+              <Icon> 
+                <FaReact /> 
+                </Icon> 
+                React 
+                </TechnologyItem>
+            <TechnologyItem>
+              <Icon>
+                <BiLogoRedux />
+                </Icon>
+                Redux-Toolkit
+                </TechnologyItem>
+            <TechnologyItem>
+              <Icon>
+                <TbBrandTypescript />
+                </Icon>
+                TypeScript
+                </TechnologyItem>
+                <TechnologyItem>
+              <Icon>
+                <SiTailwindcss />
+                </Icon>
+                TailWind CSS
+                </TechnologyItem>
+          </TechnologiesList>
+        </div>
+
+  
+        <div>
+          <TechnologiesSubTitle> Backend </TechnologiesSubTitle>
+          <TechnologiesList>
+            <TechnologyItem>
+              <Icon>
+                <FaNodeJs />
+                </Icon>
+                Node.js
+                </TechnologyItem>
+            <TechnologyItem>
+              <Icon>
+                <FaPhp />
+                </Icon>
+                PHP
+                </TechnologyItem>
+                <TechnologyItem>
+              <Icon>
+                <BiLogoFirebase />
+                </Icon>
+                Firebase
+                </TechnologyItem>
+            <TechnologyItem>
+              <Icon>
+                <SiMongodb />
+                </Icon>
+                MongoDB
+                </TechnologyItem>
+          </TechnologiesList>
+        </div>
+      </TechbologyListsContainer>
       </AboutContent>
     </AboutContainer>
   );
--- src/components/sections/about/about.styles.js
+++ src/components/sections/about/about.styles.js
@@ -1,25 +1,24 @@
 import styled from "styled-components";
 
 export const AboutContainer = styled.div`
-  background-color: rgba(255, 255, 255, 0.6); /* Adjust the last value for opacity */
+  background-color: rgba(255, 255, 255, 0.6);
   padding: 50px 0;
   margin-bottom: 15px;
   border-radius: 10px;
-  box-shadow: 0 0 20px rgba(0, 0, 0, 0.1); /* Add a subtle box shadow for depth */
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
 `;
 
 export const AboutContent = styled.div`
   max-width: 800px;
   margin: 0 auto;
   text-align: center;
+  padding-top: 20px;
 `;
 
 export const AboutText = styled.p`
   font-size: 16px;
   line-height: 1.6;
   color: #333;
-
-
 `;
 
 export const ProfileImage = styled.img`
@@ -40,5 +39,29 @@ export const TechnologiesList = styled.ul`
 
 export const TechnologiesTitle = styled.p`
   font-weight: bold;
+  font-size: 20px;
   margin-bottom: 10px;
+`;
+
+export const TechnologiesSubTitle = styled.p`
+  font-weight: bold;
+  margin-bottom: 10px;
+  text-decoration: underline;
+`;
+
+export const Icon = styled.span`
+  margin-right: 10px;
+`;
+
+export const TechnologyItem = styled.li`
+  display: flex;
+  align-items: center;
+  margin-bottom: 8px;
+`;
+
+export const TechbologyListsContainer = styled.div`
+  display: flex;
+  margin: 0 auto;
+  max-width: 800px;
+  justify-content: space-around;
 `;
