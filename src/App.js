225a4ea998f1a0b77af9aa7d71a3d172c7f03028
Larry Motuzis <larrymotuzis@Larrys-MacBook-Pro-2.local>
2024-01-09 17:10:31 -0600

    update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Main from './components/Main';
 
 function App() {
   return (
     <div className="App">
       <h1> Larry Motuzis</h1>
-      <Router>
-        <Routes>
-        <Route path='/' element={<Main />}/>
-        </Routes>
-      </Router>
+      <Main />
     </div>
   );
 }
